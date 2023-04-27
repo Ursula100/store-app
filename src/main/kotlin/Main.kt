2c55adea25958f40bcaa28c48ca308d24db477cb
tamen <20100902@mail.wit.ie>
2023-04-27 13:46:58 +0100

    Add skeleton menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-}
+fun main() = mainMenu()
+fun mainMenu() = print(
+         """ 
+         > -----------------------------------------------------  
+         > |                  NOTE KEEPER APP                  |
+         > -----------------------------------------------------  
+         > | NOTE MENU                                         |
+         > |   1) Add a Collection                             |
+         > |   2) List Collections                             |
+         > |   3) Update a Collection                          |
+         > |   4) Delete a Collection                          |
+         > -----------------------------------------------------   
+         > ==>> """.trimMargin(">"))
