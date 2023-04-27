a65a323bcc3a0ef2ebe8fe2fbd01017086c8b4ec
tamen <20100902@mail.wit.ie>
2023-04-27 13:59:02 +0100

    Edit menu to accept input from user. Use utilities added to app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
-fun main() = mainMenu()
-fun mainMenu() = print(
-         """ 
+import utils.ScannerInput
+import utils.ScannerInput.readNextInt
+
+fun main() = runMenu()
+
+fun runMenu() {
+    do {
+        when (val option = mainMenu()) {
+            1 -> addCollection()
+            2 -> listCollections()
+            3 -> updateCollection()
+            4 -> deleteCollection()
+            0 -> exitApp()
+            else -> println("Invalid menu choice: $option")
+        }
+    } while (true)
+}
+fun mainMenu() = readNextInt(
+    """ 
          > -----------------------------------------------------  
          > |                  NOTE KEEPER APP                  |
          > -----------------------------------------------------  
@@ -10,4 +26,16 @@ fun mainMenu() = print(
          > |   3) Update a Collection                          |
          > |   4) Delete a Collection                          |
          > -----------------------------------------------------   
-         > ==>> """.trimMargin(">"))
+         > ==>> """.trimMargin(">")
+)
+
+fun addCollection() = println("Adds a Collection")
+
+fun listCollections() = println("List of Collections")
+
+fun updateCollection() = println("Updates a Collection")
+
+fun deleteCollection() = println("Deletes a Collection")
+
+fun exitApp() = println("Exiting...")
+
