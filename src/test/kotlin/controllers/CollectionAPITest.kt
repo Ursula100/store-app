e7a9b4866593ddd42e7d3ae53d9c52ad3365f211
tamen <20100902@mail.wit.ie>
2023-05-02 15:15:24 +0100

    Add test for adding Collection in CollectionAPITest

---
+++ src/test/kotlin/controllers/CollectionAPITest.kt
@@ -0,0 +1,62 @@
+package controllers
+
+import models.Collection
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Nested
+
+class CollectionAPITest {
+    private var summer: Collection? = null
+    private var daizy: Collection? = null
+    private var justMe: Collection? = null
+    private var feezyRitz: Collection? = null
+    private var populatedCollection: CollectionAPI? = CollectionAPI()
+    private var emptyCollection: CollectionAPI? = CollectionAPI()
+
+    @BeforeEach
+    fun setup() {
+        summer = Collection(cname = "Summer", createdBy = "Me", rank = 1)
+        daizy = Collection(cname = "Daizy", createdBy = "Charles Dave", rank = 1)
+        justMe = Collection(cname = "JustMe", createdBy = "Penelope Wen", rank = 1)
+        feezyRitz = Collection(cname = "FeezyRitz", createdBy = "Ritz Dumblemort", rank = 1)
+
+        //adding collections to populatedCollection
+        populatedCollection!!.add(summer!!)
+        populatedCollection!!.add(daizy!!)
+        populatedCollection!!.add(justMe!!)
+        populatedCollection!!.add(feezyRitz!!)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        summer = null
+        daizy = null
+        justMe = null
+        feezyRitz = null
+        populatedCollection = null
+        emptyCollection = null
+    }
+
+    @Nested
+    inner class AddCollection {
+        @Test
+        fun `adding a Collection to a populated list adds to the ArrayList`() {
+            val newCollection = Collection(cname = "WinterTrend", createdBy = "Me", rank = 1)
+            assertEquals(4, populatedCollection!!.numberOfCollections())
+            assertTrue(populatedCollection!!.add(newCollection))
+            assertEquals(5, populatedCollection!!.numberOfCollections())
+            assertEquals(newCollection, populatedCollection!!.findCollection(populatedCollection!!.numberOfCollections() - 1))
+        }
+
+        @Test
+        fun `adding a Collection to an empty list adds to the ArrayList`() {
+            val newCollection = Collection(cname = "WinterTrend", createdBy = "Me", rank = 1)
+            assertEquals(0, emptyCollection!!.numberOfCollections())
+            assertTrue(emptyCollection!!.add(newCollection))
+            assertEquals(1, emptyCollection!!.numberOfCollections())
+            assertEquals(newCollection, emptyCollection!!.findCollection(emptyCollection!!.numberOfCollections() - 1))
+        }
+    }
+}
