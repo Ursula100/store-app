48185166e36b568141a9f0eaf2d4cb23deeb8800
tamen <20100902@mail.wit.ie>
2023-05-02 16:35:46 +0100

    Update listAllCollections in MainKT, override toString() and add numberOfItems() method to Collection

--- src/main/kotlin/models/Collection.kt
+++ src/main/kotlin/models/Collection.kt
@@ -3,4 +3,9 @@ package models
 import java.time.LocalDate
 import java.util.*
 
-class Collection(var collectionId: Int = 2000, var cname: String, var createdBy: String, var rank: Int, var isEmpty: Boolean = true, var items: MutableSet<Item> = mutableSetOf()) {}
+class Collection(var collectionId: Int = 2000, var cname: String, var createdBy: String, var rank: Int, var isEmpty: Boolean = true, var items: MutableSet<Item> = mutableSetOf()) {
+   fun numberOfItems() =  items.size
+    override fun toString(): String {
+        return "Id - $collectionId: ${cname.toUpperCase()} by ${createdBy.toUpperCase()}. Contains ${numberOfItems()} items"
+    }
+}
