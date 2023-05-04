ab809427dc4527849f341a30f46bd9d4200247ff
tamen <20100902@mail.wit.ie>
2023-05-04 13:39:13 +0100

    Dependencies including Jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    // Plugin for Dokka - KDoc generating tool
+    id("org.jetbrains.dokka") version "1.6.10"
+    jacoco
     application
 }
 
@@ -14,10 +17,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // For generating a Dokka Site from KDoc
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
 }
 
 tasks.test {
     useJUnitPlatform()
+    //report is always generated after tests run
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.withType<KotlinCompile> {
